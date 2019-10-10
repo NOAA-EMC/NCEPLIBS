fa3013128ebc160cf98b808ccefc347bd7fdcc96
Mark A Potts <Mark.Potts@noaa.gov>
2019-10-10 17:42:24 +0000

    updated to include post, crtm and gsi

--- NCEPLIBS-crtm/build/CMakeFiles/copy.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/copy"
-  "../CRTM_Module.F"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/copy.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
