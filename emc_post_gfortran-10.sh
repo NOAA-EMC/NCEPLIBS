62501efded53b4d68d811d8353235cc57b98c374
Dom Heinzeller <dom.heinzeller@noaa.gov>
2020-10-08 20:26:07 -0600

    Ignore that the emc_post patch for gfortran-10 has been applied previously and proceed

--- emc_post_gfortran-10.sh
+++ emc_post_gfortran-10.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-set -ex
+set -x
 
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $MYDIR
-patch -p0 < emc_post_gfortran-10.patch 
+patch -N -p0 < emc_post_gfortran-10.patch
+exit 0
